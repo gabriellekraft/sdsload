66aaeaffa5eb4588b2443a6d474a10e232450a5c
Gabrielle Kraft <kraftgabrielle@gmail.com>
2025-05-08 08:47:25 -0400

    data doc update

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,7 @@
-## code to prepare `DATASET` dataset goes here
-
-usethis::use_data(DATASET, overwrite = TRUE)
+class_packages <- list(
+  sds_291 = c("Stat2Data", "ggplot2", "broom", "dplyr", "equatiomatic", "performance", "see", "gridExtra", "moderndive", "GGally", "infer", "emmeans"),
+  sds_100 = c("caret", "randomForest", "glmnet", "skimr"),
+  sds_192 = c("BiocManager", "DESeq2", "janitor", "ggpubr"),
+  sds_220 = c("tidyverse", "lubridate", "stringr", "readxl")
+)
+usethis::use_data(class_packages, overwrite = TRUE)
