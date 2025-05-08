054b349459a6853d79072d035015662aab697450
Gabrielle Kraft <kraftgabrielle@gmail.com>
2025-05-08 18:46:12 -0400

    Removed package

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,6 +2,6 @@ class_packages <- list(
   sds_291 = c("Stat2Data", "ggplot2", "broom", "dplyr", "equatiomatic", "performance", "see", "gridExtra", "moderndive", "GGally", "infer", "emmeans", "tidyverse"),
   sds_100 = c("openintro", "moderndive", "ggplot2", "dplyr", "tidyverse", "usethis", "mosaicData", "palmerpenguins", "fivethirtyeight", "janitor", "readr", "knitr" ),
   sds_192 = c("dplyr", "leaflet", "ggplot2", "ggpubr", "tidyverse", "lubridate", "openintro", "infer", "moderndive", "knitr", "readr", "tidyr"),
-  sds_220 = c("ggplot2", "tidyverse", "dplyr", "moderndive", "infer", "readr", "tidyr", "gapminder", "skimr", "purrr", "tibble", "stringer", "forcats")
+  sds_220 = c("ggplot2", "tidyverse", "dplyr", "moderndive", "infer", "readr", "tidyr", "gapminder", "skimr", "purrr", "tibble", "forcats")
 )
 usethis::use_data(class_packages, overwrite = TRUE)
