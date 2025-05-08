9fe1fd329e7240081efe6d0c9192593cb192367a
Sofija Stankovic <sstankovic@smith.edu>
2025-05-08 16:08:51 -0400

    /

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,7 +1,7 @@
 class_packages <- list(
-  sds_291 = c("Stat2Data", "ggplot2", "broom", "dplyr", "equatiomatic", "performance", "see", "gridExtra", "moderndive", "GGally", "infer", "emmeans"),
-  sds_100 = c("caret", "randomForest", "glmnet", "skimr"),
-  sds_192 = c("BiocManager", "DESeq2", "janitor", "ggpubr"),
+  sds_291 = c("Stat2Data", "ggplot2", "broom", "dplyr", "equatiomatic", "performance", "see", "gridExtra", "moderndive", "GGally", "infer", "emmeans", "tidyverse"),
+  sds_100 = c("openintro", "moderndive", "ggplot2", "dplyr", "tidyverse"),
+  sds_192 = c("dplyr", "leaflet", "ggplot2", "ggpubr", "tidyverse", "lubridate"),
   sds_220 = c("tidyverse", "lubridate", "stringr", "readxl")
 )
 usethis::use_data(class_packages, overwrite = TRUE)
